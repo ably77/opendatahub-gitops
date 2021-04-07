880f1f10df4649eaf93b066a97feeb1dc793c5f6
ably77 <alexbrucely@gmail.com>
2021-04-07 13:52:33 -0700

    add namespace

--- scripts/runme.sh
+++ scripts/runme.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+GITHUB_USERNAME="ably77"
+
 # create argocd operator
 echo now deploying argoCD
-until oc apply -k https://github.com/ably77/openshift-testbed-apps/kustomize/instances/overlays/operators/namespaced-operators/argocd-operator; do sleep 2; done
+until oc apply -k https://github.com/${GITHUB_USERNAME}/openshift-testbed-apps/kustomize/instances/overlays/operators/namespaced-operators/argocd-operator; do sleep 2; done
 
 # wait for argo cluster rollout
 ./scripts/wait-for-rollout.sh deployment argocd-server argocd 10
 
 ### deploy odh operator in argocd
 echo deploying opendatahub operator
-oc apply -f https://raw.githubusercontent.com/ably77/opendatahub-gitops/main/odh-argoapp.yaml
+oc apply -f https://raw.githubusercontent.com/${GITHUB_USERNAME}/opendatahub-gitops/main/odh-argoapp.yaml
 
 ### check kafka deployment status
 #echo waiting for kafka deployment to complete
